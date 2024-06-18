4944c01c48e9b24712d4f4e32a7fbdf78f72b2e8
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-18 16:04:52 +0200

    [upd] bootsrap imported in project and style structure started

--- index.php
+++ index.php
@@ -47,20 +47,34 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>php hotel</title>
 </head>
 
 <body>
-    <p>
-        <?php
-        //per ogni oggetto presente all interno dell array associativo stampiamo chiave e valore
-        foreach ($hotels as $hotel) {
-            foreach ($hotel as $key => $value) {
-                echo $key . ': ' . $value . '<br>';
-            }
-        }
-        ?>
-    </p>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 bg-dark text-center rounded my-3 py-2 text-white">
+                <h1 class="mb-3">B&B</h1>
+                <p>find your next adventure</p>
+            </div>
+            <div class="col-12 bg-info">
+                <p>
+                    <?php
+                    //per ogni oggetto presente all interno dell array associativo stampiamo chiave e valore
+                    foreach ($hotels as $hotel) {
+                        foreach ($hotel as $key => $value) {
+                            echo $key . ': ' . $value . '<br>';
+                        }
+                    }
+                    ?>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
